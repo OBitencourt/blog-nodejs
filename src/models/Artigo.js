36d810c71bb720699f7e4cf885f1df56f90c7ec0
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2026-04-26 14:24:47 +0100

    feat: Adicionando models

---
+++ src/models/Artigo.js
@@ -0,0 +1,23 @@
+const mongoose = require("mongoose");
+
+const artigoSchema = new mongoose.Schema(
+  {
+    titulo: { type: String, required: true, trim: true },
+    conteudo: { type: String, required: true },
+    categoria: {
+      type: String,
+      required: true,
+      enum: ["tecnologia", "ciência", "desporto", "cultura", "outro"],
+    },
+    autor: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Utilizador",
+      required: true,
+    },
+    publicado: { type: Boolean, default: false },
+    imagem: { type: String },
+  },
+  { timestamps: true },
+);
+
+module.exports = mongoose.model("Artigo", artigoSchema);
