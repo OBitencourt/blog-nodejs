f731c4d7b9b220a3884251e861464d5a49a3ea94
Arthur Bitencourt <arthurbitencourtvsilva@gmail.com>
2026-04-26 14:43:06 +0100

    feat: Adicionando detalhes finais e versão final do server.js

--- src/server.js
+++ src/server.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const dotenv = require("dotenv");
+const cors = require("cors")
 const connectDB = require("./config/db");
-
 dotenv.config();
+
 connectDB();
-
 const app = express();
-
+app.use(cors())
 app.use(express.json());
 
-app.get("/", (req, res) => {
-  res.json({ mensagem: "API Blog online!" });
+// Rotas da API
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/artigos", require("./routes/artigos"));
+app.use("/api/artigos", require("./routes/comentario"));
+app.use("/api/comentarios", require("./routes/comentario"));
+
+// Rota não encontrada
+
+app.use((req, res) => {
+  res.status(404).json({ erro: "Rota não encontrada" });
 });
-// Rotas da API (adicionar nas fases seguintes)
-// app.use('/api/auth', require('./routes/auth'));
-// app.use('/api/artigos', require('./routes/artigos'));
-// Middleware de erros (deve ser o último)
-// app.use(require('./middleware/errorHandler'));
+
+// Middleware global de erros
+app.use(require("./middleware/errorHandler"));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Servidor a correr na porta ${PORT}`);
